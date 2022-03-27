098d19a2899fe9d6c2a6a939f570468c54822e11
Jparw <jparw3@gmail.com>
2022-03-27 21:59:20 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+    <Head>
+      <title>Research Titles Generator</title>
+      <meta name="description" content="This tool randomly generates research title ideas for your reference or inspiration.
+        Currently, the generated titles are for.
+" />
+    </Head>
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
